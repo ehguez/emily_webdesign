b87b2d24cef8862ace1db88e508958521356b8f5
Emily <eguez@risd.edu>
2018-02-05 16:26:57 -0500

    updates
    --

--- computer-collab/js/script.js
+++ computer-collab/js/script.js
@@ -173,52 +173,7 @@ $(document).ready(function(){
 
 
 
-    var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
-
-    // Update the count down every 1 second
-    var x = setInterval(function() {
-
-    // Get todays date and time
-    var now = new Date().getTime();
-
-    // Find the distance between now an the count down date
-    var distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Display the result in the element with id="demo"
-    document.getElementById("clock").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-
-    // If the count down is finished, write some text 
-    if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("clock").innerHTML = "EXPIRED";
-    }
-    }, 1000);
-
-
-    /*$("#startClock").click(function() {
-    var counter = 5;
-    setInterval(function() {
-     counter--;
-      if (counter >= 0) {
-         span = document.getElementById("timeOptions");
-         span.innerHTML = counter;
-      }
-      if (counter === 0) {
-         alert('sorry, out of time');
-         clearInterval(counter);
-       }
-     }, 1000);*/
 });
-
-
-
 
 
 
